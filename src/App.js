a129cba1f9d4b33cbd513c12dcbb8d49116208be
BeckoUK <veselin91georgiev@gmail.com>
2020-12-13 14:00:10 +0000

    ReactPlayer and bits

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import VideosDocumentaries from './components/pages/VideosDocumentaries';
 import Articles from './components/pages/Articles';
 import SignUp from './components/pages/SignUp';
 import Questionaires from './components/pages/Questionaire';
+import ReactPlayer from "react-player"
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/videosdocumentaries' component={VideosDocumentaries} />
+          <Route path='/videosdocumentaries' component={VideosDocumentaries}/>
           <Route path='/questionaires' component={Questionaires} />
           <Route path='/articles' component={Articles} />
           <Route path='/sign-up' component={SignUp} />
--- src/components/pages/VideosDocumentaries.js
+++ src/components/pages/VideosDocumentaries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 
+
 export default function VideosDocumentaries() {
   return <h1 className='videosdocumentaries'>Videos & Documentaries</h1>;
 }
