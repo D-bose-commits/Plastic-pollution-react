3acec9ff7b68d15e34cab9b9107de0ce5652814c
BeckoUK <veselin91georgiev@gmail.com>
2020-12-13 18:35:34 +0000

    Update VideosDocumentaries.js

--- src/components/pages/VideosDocumentaries.js
+++ src/components/pages/VideosDocumentaries.js
@@ -3,24 +3,27 @@ import '../../App.css';
 import ReactPlayer from "react-player"
 
 function VideosDocumentaries() {
- return <div className="VideosDocumentaries">
- Plastic Polution Video 1
-    
-         <ReactPlayer
-         url="https://www.youtube.com/watch?v=IA9O9YUbQew"
+    return <div className="VideosDocumentaries">
+            <div className = "ReactPlayer">
+             <h1>Plastic Polution Videos </h1> 
+            <ReactPlayer className="VideoPlayer" url="https://www.youtube.com/watch?v=IA9O9YUbQew" />
+        </div>
+        <div className="ReactPlayer">
+            <h1>Plastic Polution Videos </h1>
+            <ReactPlayer className="VideoPlayer" url="https://www.youtube.com/watch?v=HQTUWK7CM-Y" />
+        </div>
+        <div className="ReactPlayer">
+            <h1>Plastic Polution Videos </h1>
+            <ReactPlayer className="VideoPlayer" url="https://www.youtube.com/watch?v=Yomf5pBN8dY" />
+        </div>
         
-     />plastic polution video 2
-     <ReactPlayer
-         url="https://www.youtube.com/watch?v=HQTUWK7CM-Y"
-     />
-     Plastic poluton video 3
-     <ReactPlayer
-         url="https://www.youtube.com/watch?v=Yomf5pBN8dY"
-     />
-
+    <div className="ReactPlayer">
+            <h1>Plastic Polution Documentary
+                </h1>
+            <ReactPlayer className="VideoPlayer" url="https://www.youtube.com/watch?v=9-dpv2xbFyk" />
+        </div>
 
      </div>
-    
-     
+ 
 }
 export default VideosDocumentaries; 
