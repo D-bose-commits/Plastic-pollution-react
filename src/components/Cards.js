ab8a9c339435cedc76f94c28d3dff170f57d1f96
Jehquisa <62106224+jehquisab@users.noreply.github.com>
2020-12-14 12:31:37 +0000

    Add files via upload

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,9 +17,9 @@ function Cards() {
             />
             <CardItem
               src='images/img-2.jpg'
-              text='Take our questionaire and see if your doing enough for the enviroment'
-              label='Questionaires'
-              path='/questionaires'
+              text="Take one of our quizzes and see if you're doing enough for the environment"
+              label='Questionnaires'
+              path='/questionnaires'
             />    
             <CardItem
               src='images/img-3.jpg'
