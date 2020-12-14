6449aa18359d72ffc9ab945e851074732f8da316
Jehquisa <62106224+jehquisab@users.noreply.github.com>
2020-12-14 22:15:48 +0000

    link check and edit

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -4,8 +4,13 @@ import Iframe from 'react-iframe'
 
 export default function Questionnaires() {
   return <>
+<<<<<<< Updated upstream
   <div className='questionnaire'>
   <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSft6SAfbUqmH1cvrc39FDGjXwNvWx7ppNosjFlN6N-e0L794g/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0"/>
+=======
+  <div className='questionnaires'>
+  <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSfQwCnuC3HDjt3AxOJ-KGhDU6tXJMQL8l274JxPl7_pfJChJg/viewform?embedded=true" width="640" height="6350" frameborder="0" marginheight="0" marginwidth="0"/>
+>>>>>>> Stashed changes
   </div>
   </>
 }
