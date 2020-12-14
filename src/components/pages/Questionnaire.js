99bce5d1359fac488f9c5166cd46372e9ce8f387
D-bose-commits <75704221+D-bose-commits@users.noreply.github.com>
2020-12-14 02:19:51 +0000

    Add files via upload

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../../App.css';
+import Iframe from 'react-iframe'
 
 export default function Questionnaires() {
-  return <h1 className='questionnaires'>Quizzes</h1>;
+  return <>
+  <div className='questionnaire'>
+  <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSft6SAfbUqmH1cvrc39FDGjXwNvWx7ppNosjFlN6N-e0L794g/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0"/>
+  </div>
+  </>
 }
