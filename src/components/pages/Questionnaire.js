f5307c33e53fe0f1212189ddb243f6b0ffeb0207
Jehquisa <62106224+jehquisab@users.noreply.github.com>
2020-12-31 19:31:21 +0000

    questionnaire fix

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import Iframe from 'react-iframe'
+import Footer from '../Footer';
+
 
 export default function Questionnaires() {
   return <>
-<<<<<<< Updated upstream
-  <div className='questionnaire'>
-  <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSft6SAfbUqmH1cvrc39FDGjXwNvWx7ppNosjFlN6N-e0L794g/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0"/>
-=======
   <div className='questionnaires'>
-  <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSfQwCnuC3HDjt3AxOJ-KGhDU6tXJMQL8l274JxPl7_pfJChJg/viewform?embedded=true" width="640" height="6350" frameborder="0" marginheight="0" marginwidth="0"/>
->>>>>>> Stashed changes
+  <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSft6SAfbUqmH1cvrc39FDGjXwNvWx7ppNosjFlN6N-e0L794g/viewform?embedded=true" width="640" height="800" frameborder="0" marginheight="0" marginwidth="0"/>
   </div>
+  <Footer />
   </>
 }
