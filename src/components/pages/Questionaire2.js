d640191f667d7feafaad3e4950345c5458694c4b
Nikhil Kerai <73474594+1906218@users.noreply.github.com>
2021-01-25 11:39:19 +0000

    Update Questionaire2.js

--- src/components/pages/Questionaire2.js
+++ src/components/pages/Questionaire2.js
@@ -7,7 +7,7 @@ export default function Questionnaires() {
   
   return <>
     <div className='questionnaire'>
-    <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSflY0i9I9xLBuAraGqQ0bLZHXHNRA5gt0pmMjtyDF8yeyJ1LA/viewform?embedded=true" width="640" height="1414" frameborder="0" marginheight="0" marginwidth="0"/>
+    <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSflY0i9I9xLBuAraGqQ0bLZHXHNRA5gt0pmMjtyDF8yeyJ1LA/viewform?embedded=true" width="645" height="1414" frameborder="0" marginheight="0" marginwidth="0"/>
     </div>
     </>
   }
