aad5e7cac36f34bb4a781e7fba6c9b5e3a5a86fe
Jehquisa <62106224+jehquisab@users.noreply.github.com>
2021-02-15 14:47:31 +0000

    changes

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -7,11 +7,7 @@ export default function Questionnaires() {
   
   return <>
     <div className='questionnaire'>
-<<<<<<< Updated upstream
-    <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSfQwCnuC3HDjt3AxOJ-KGhDU6tXJMQL8l274JxPl7_pfJChJg/viewform?embedded=true" width="640" height="6350" frameborder="0" marginheight="0" marginwidth="0"/>
-=======
     <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSfQwCnuC3HDjt3AxOJ-KGhDU6tXJMQL8l274JxPl7_pfJChJg/viewform?embedded=true" width="640" height="1414" frameborder="0" marginheight="0" marginwidth="0"/>
->>>>>>> Stashed changes
     </div>
     </>
   }
