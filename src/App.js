661de9c6f74b2e1eeeb37f96568e52552fc0d0dc
Nikhil Kerai <73474594+1906218@users.noreply.github.com>
2021-02-16 15:50:21 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,36 +8,5 @@ import Articles from './components/pages/Articles';
 // import SignUp from './components/pages/SignUp';
 import Signup from './user/signup/Signup';
 import Login from './user/login/Login';
-
-
-import yes from './app/yes';
-
-import Questionnaires from './components/pages/Questionnaire';
-
-import ReactPlayer from "react-player"
-
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/videosdocumentaries' component={VideosDocumentaries}/>
-          <Route path='/questionnaires' component={Questionnaires} />
-          <Route path='/articles' component={Articles} />
-          <Route path='/sign-up' component={Signup} /> 
-          <Route path='/login' component={Login} />
-          
-          {/* <Route path='/sign-up' component={SignUp} /> */}
-
-        </Switch>
-      </Router>
-    </>
-  );
-}
-
-export default App;
-
-//Npm install , npm install react-router-dom, npm i react-player, npm i react-iframe
+import Quiz2 from './components/pages/Quiz2';
+import Quiz3 from './components/pages/Quiz3';
