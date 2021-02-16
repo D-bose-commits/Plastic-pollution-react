cce9ad61b8fbb74b3a5f4a47b254c6f2003ce696
Nikhil Kerai <73474594+1906218@users.noreply.github.com>
2021-02-16 15:41:29 +0000

    Add files via upload

--- src/components/pages/Quiz2.js
+++ src/components/pages/Quiz2.js
@@ -100,3 +100,29 @@ export default function App() {
 			setShowScore(true);
 		}
 	};
+	
+	
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
