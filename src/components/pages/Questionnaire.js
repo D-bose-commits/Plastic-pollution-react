995d55f149e95a5d91091819063b00e3e9e97983
Imad jahangir <60513884+imad143@users.noreply.github.com>
2021-02-18 17:47:07 +0000

    showiing thhe problem
    import Quiz3 from './components/pages/Quiz3';
    
    import Questionnaires from './components/pages/Questionnaire';
    
     <Route path='/Quiz3' component={Quiz3} />
    
    i delete theses things and after that website start runs

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -5,6 +5,4 @@ import '../../App.css';
     <div className='questionnaire'>
     <Iframe url="https://docs.google.com/forms/d/e/1FAIpQLSfQwCnuC3HDjt3AxOJ-KGhDU6tXJMQL8l274JxPl7_pfJChJg/viewform?embedded=true" width="640" height="1414" frameborder="0" marginheight="0" marginwidth="0"/>
     </div>
-    </>
-  }
-
+    
