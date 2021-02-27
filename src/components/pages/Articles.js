9019a531238599db6e26bdcad0c6b9586c3c5362
BeckoUK <veselin91georgiev@gmail.com>
2021-02-27 00:46:42 +0000

    s
    s

--- src/components/pages/Articles.js
+++ src/components/pages/Articles.js
@@ -2,5 +2,63 @@ import React from 'react';
 import '../../App.css';
 
 export default function Articles() {
+<<<<<<< Updated upstream
   return <h1 className='articles'>Articles</h1>;
+=======
+  return  <>
+          <div className='articles'>
+
+            <div className='articles-wrapper1'>
+            <h1>Articles on Plastic Pollution</h1>
+            <Link to="https://ourworldindata.org/plastic-pollution?utm_source=newsletter">Plastic pollution - www.ourworlddata.com</Link>
+            <div className='articles-wrapper1'>
+            <Link to='http://www.int-res.com/articles/meps/37/m037p295.pdf'>Lel </Link>
+            </div>
+            <div className='articles-wrapper1'>
+          
+            Inadequately disposed waste is that which has the intention of being managed through waste collection or storage sites, but is ultimately not formally or sufficiently managed. This includes disposal in dumps or open, uncontrolled landfills; this means the material is not fully contained and can be lost to the surrounding environment. This makes it at risk of leakage and transport to the natural environment and oceans via waterways, winds and tides.
+
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.sciencedirect.com/science/article/abs/pii/S0269749117334310'>Plastic pollution in islands of the Atlantic Ocean - www.sciencedirect.com </Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.pnas.org/content/114/38/9994.short'>Threat of plastic pollution to seabirds is global - www.pnas.org</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.sciencedirect.com/science/article/abs/pii/S0025326X12006224'>Plastic pollution in the South Pacific - www.sciencedirect.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+            <div className='articles-wrapper'>
+            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+            </div>
+
+           </div>
+
+          </div>
+          <Footer/>
+</>
+
+
+>>>>>>> Stashed changes
 }
