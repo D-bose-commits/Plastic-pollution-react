4443dd7ee9cfced79e28cdbefe9f9e032370425b
Daniel Bose -1613380 <75704221+D-bose-commits@users.noreply.github.com>
2021-03-01 07:59:56 +0000

    Add files via upload

--- src/components/pages/Articles.js
+++ src/components/pages/Articles.js
@@ -18,11 +18,18 @@ export default function Articles() {
             
             <a href="http://www.int-res.com/articles/meps/37/m037p295.pdf" rel=">Marine birds and plastic pollution - www.int-res.com">
             Marine birds and plastic pollution - www.int-res.com</a>
-            <Link to='https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0111913&xid=17259,15700021,15700124,15700149,15700168,15700173,15700186,15700191,15700201'>Plastic Pollution in the World's Ocean - https://journals.plos.org/</Link>
-            <Link to='https://www.sciencedirect.com/science/article/abs/pii/S0269749117334310'>Plastic pollution in islands of the Atlantic Ocean - www.sciencedirect.com </Link>
-            <Link to='https://www.pnas.org/content/114/38/9994.short'>Threat of plastic pollution to seabirds is global - www.pnas.org</Link>
-            <Link to='https://www.sciencedirect.com/science/article/abs/pii/S0025326X12006224'>Plastic pollution in the South Pacific - www.sciencedirect.com</Link>
-            <Link to='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</Link>
+              <br></br>
+
+            <a href='https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0111913&xid=17259,15700021,15700124,15700149,15700168,15700173,15700186,15700191,15700201'>Plastic Pollution in the World's Ocean - https://journals.plos.org/</a>
+            <br></br>
+            <a href='https://www.sciencedirect.com/science/article/abs/pii/S0269749117334310'>Plastic pollution in islands of the Atlantic Ocean - www.sciencedirect.com </a>
+            <br></br>
+            <a href='https://www.pnas.org/content/114/38/9994.short'>Threat of plastic pollution to seabirds is global - www.pnas.org</a>
+            <br></br>
+            <a href='https://www.sciencedirect.com/science/article/abs/pii/S0025326X12006224'>Plastic pollution in the South Pacific - www.sciencedirect.com</a>
+            <br></br>
+            <a href='https://www.nature.com/articles/544297a/'>Sea change for plastic pollution - www.nature.com</a>
+            <br></br>
             <li className="articles-wrapper">Section 2</li>
             <li className="articles-wrapper">Section 3</li>
             <li className="articles-wrapper">Section 4</li>
