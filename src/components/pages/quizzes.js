e12ae7557508c62628cbb16e3ee408e7a725acec
Jehquisa - 1935082 <62106224+jehquisab@users.noreply.github.com>
2021-03-12 03:57:34 +0000

    Update quizzes.js

--- src/components/pages/quizzes.js
+++ src/components/pages/quizzes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../App.css';
+//import Quiz1 from './Quiz1';
 import Quiz2 from './Quiz2';
 import Quiz3 from './Quiz3';
-import Quiz4 from './Quiz4';
+
 import Footer from '../Footer';
 
 function quizzes() {
   return (
     <>
-      <Quiz2 /> <Quiz4 />
+      <Quiz2 /> 
       <Quiz3 />
       
       <Footer />
