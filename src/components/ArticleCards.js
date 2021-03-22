4e4fec5ff9c0176507f5f26b2e7f8cf9cb1486b6
Jehquisa - 1935082 <62106224+jehquisab@users.noreply.github.com>
2021-03-22 12:43:02 +0000

    Update ArticleCards.js

--- src/components/ArticleCards.js
+++ src/components/ArticleCards.js
@@ -23,7 +23,7 @@ function ArticleCard() {
             />    
             <CardItem
               src='images/img-8.jpg'
-              text='Studies Detailing more in depth information about the current situation on Pollution'
+              text='Studies detailing more in depth information about the current situation on Pollution'
               label='Scholar Articles'
               path='/Pollutionscholarly'
             />
